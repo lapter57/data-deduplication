ead928589fe78c058064ca2f04a00d6803e11ba1
Egor Shakhmin <lapter57@gmail.com>
2021-10-12 10:42:35 +0300

    [#1] create project (#2)

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("application")
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+}
+
+group = "ru.spbstu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
+    val lombok = "org.projectlombok:lombok:1.18.22"
+    compileOnly(lombok)
+    annotationProcessor(lombok)
+    testCompileOnly(lombok)
+    testAnnotationProcessor(lombok)
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
