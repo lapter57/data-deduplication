58cccaa69139249c1943d2fdf620621a103ad6ad
AstralRomance <andrdoncwork@mail.ru>
2021-12-05 17:43:26 +0300

    docker-compose generation reworked.

--- metrics/main.py
+++ metrics/main.py
@@ -2,43 +2,35 @@ import os
 import os.path as path
 import subprocess
 
+import requests
 import ruamel.yaml
+import pathlib
 
 
 GET_MONGO_SIZE_COMMAND = 'docker-compose exec mongodb mongo data_deduplication --quiet --eval "db.stats()"'
+APP_URL = 'http://localhost:8080'
 
 work_dir = path.abspath(path.join(__file__, '../../'))
 os.chdir(work_dir)
 
-
-app_ports = ruamel.yaml.scalarstring.DoubleQuotedScalarString('8080:8080')
-mongo_ports = ruamel.yaml.scalarstring.DoubleQuotedScalarString('27017:27017')
+source_compose = ruamel.yaml.YAML(typ='safe')
+with open('docker-compose.yml', 'r') as src_f:
+    source_compose = ruamel.yaml.load(src_f)
 block_size = 256
-while block_size <= 131072:
-    docker_compose = {'version': '3'}
-    services = {
-        'mongodb': {
-            'image': 'mongo:bionic',
-            'environment': {
-                'MONGO_INIT_DATABASE': 'data_deduplication',
-                'BLOCK_SIZE': block_size
-            },
-            'ports': [mongo_ports]
-        },
-        'app': {
-            'build': '.',
-            'expose': [8080],
-            'ports': [app_ports],
-            'depends_on': ['mongodb'],
-            'environment': ['PORT=8080','DB_URI=mongodb://mongodb:27017/data_deduplication']
-        }
-    }
-    docker_compose['services'] = services
-    with open(f'docker-compose-tmp.yml', 'w') as docker_compose_obj:
-        yaml = ruamel.yaml.YAML()
-        yaml.dump(docker_compose, docker_compose_obj)
-    get_mongo_data_command = GET_MONGO_SIZE_COMMAND.split()[0]
-    get_mongo_data_args = ' '.join(GET_MONGO_SIZE_COMMAND.split()[1::])
-    get_mongo_data = subprocess.Popen([get_mongo_data, get_mongo_data_args], stdout=subprocess.PIPE, srderr=subprocess.PIPE)
-    mongo_data, mongo_err = get_mongo_data.communicate()
+
+while block_size <= 256:
+    if len(source_compose['services']['app']['environment']) == 1:
+        source_compose['services']['app']['environment'].append(f'BLOCK_SIZE={block_size}')
+    else:
+        source_compose['services']['app']['environment'][1] = f'BLOCK_SIZE={block_size}'
+    with open('docker-compose.yml', 'w') as src_f:
+        target_compose = ruamel.yaml.YAML()
+        target_compose.dump(source_compose, src_f)
+    os.system('docker-compose up --build -d')
+    with open(path.join('metrics/test_data/test_file.txt'), 'r') as upload_file:
+        upload_data = requests.post(f'{APP_URL}/api/file', files={'test_file.txt': upload_file})
+        print(upload_data)
+    #download data
+    
+    get_mongo_data = os.system(GET_MONGO_SIZE_COMMAND)
     block_size *= 2
