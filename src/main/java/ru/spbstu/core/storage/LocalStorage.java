00514b3fe3c17f814a517246a78778e52a5135a8
Egor Shakhmin <lapter57@gmail.com>
2021-12-05 23:29:10 +0300

    create path of local storage when starting an application

--- src/main/java/ru/spbstu/core/storage/LocalStorage.java
+++ src/main/java/ru/spbstu/core/storage/LocalStorage.java
@@ -1,20 +1,28 @@
 package ru.spbstu.core.storage;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.spbstu.core.file.ByteFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
-@RequiredArgsConstructor
+@Slf4j
 @Component
 public class LocalStorage implements Storage {
     @Value("${storage.basePath}")
     private String basePath;
+
+    @PostConstruct
+    private void createBasePath() throws IOException {
+        Files.createDirectories(Paths.get(basePath));
+        log.info("Created {}", basePath);
+    }
 
     @Override
     public void save(ByteFile file, String location) {
