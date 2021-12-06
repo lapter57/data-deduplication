f46e0d58737a28505fdd29d9b84e469d136beda4
AstralRomance <andrdoncwork@mail.ru>
2021-12-06 15:47:01 +0300

    Add metric computation. Target data format needs work.

---
+++ metrics/aggregate_metrics.py
@@ -0,0 +1,40 @@
+import os
+import os.path as path
+import json
+
+def get_dir_size(dir_path: str):
+    size = 0
+    for dir_obj in os.scandir(dir_path):
+        size += os.path.getsize(dir_obj)
+    return size
+
+
+def compute_deduplication(db_info: int, block_size: int):
+    src_data_dir = path.join('test_data')
+    src_data_size = get_dir_size(src_data_dir)
+    blocks_size = get_dir_size('blocks')
+    deduplication_metric = src_data_size / (blocks_size + db_info)
+    with open(f'deduplication_info_{block_size}.json', 'w') as deduplication_outp:
+        json.dump({'block_size': block_size,
+                   'test_data_size': src_data_size,
+                   'blocks_size': blocks_size}, deduplication_outp)
+    return compute_deduplication
+
+def compute_time(metric: dict, **kwargs):
+    time_metric = {}
+    total_time = 0
+    for block_size in metric:
+        for file_type in metric[block_size]:
+            for item in metric[block_size][file_type]:
+                current_time = item['time']
+                print(current_time)
+                print(type(current_time))
+                total_time += current_time
+        time_metric[block_size] = {file_type: total_time/len(metric[block_size][file_type])}
+        total_time = 0
+    print(time_metric)
+
+
+if __name__ == '__main__':
+    #compute_deduplication(1)
+    compute_time({256: {'txt': [{'time': 0.037528, 'file_size': 6}]}})
--- metrics/main.py
+++ metrics/main.py
@@ -1,13 +1,17 @@
 import os
 import os.path as path
+import datetime
+import time
 import subprocess
+import json
 
+import magic
 import requests
 import ruamel.yaml
-import pathlib
+
+from aggregate_metrics import compute_deduplication
 
 
-GET_MONGO_SIZE_COMMAND = 'docker-compose exec mongodb mongo data_deduplication --quiet --eval "db.stats()"'
 APP_URL = 'http://localhost:8080'
 
 work_dir = path.abspath(path.join(__file__, '../../'))
@@ -17,7 +21,8 @@ source_compose = ruamel.yaml.YAML(typ='safe')
 with open('docker-compose.yml', 'r') as src_f:
     source_compose = ruamel.yaml.load(src_f)
 block_size = 256
-
+upload_metrics = {}
+download_metrics = {}
 while block_size <= 256:
     if len(source_compose['services']['app']['environment']) == 1:
         source_compose['services']['app']['environment'].append(f'BLOCK_SIZE={block_size}')
@@ -27,10 +32,32 @@ while block_size <= 256:
         target_compose = ruamel.yaml.YAML()
         target_compose.dump(source_compose, src_f)
     os.system('docker-compose up --build -d')
-    with open(path.join('metrics/test_data/test_file.txt'), 'r') as upload_file:
-        upload_data = requests.post(f'{APP_URL}/api/file', files={'test_file.txt': upload_file})
-        print(upload_data)
-    #download data
-    
-    get_mongo_data = os.system(GET_MONGO_SIZE_COMMAND)
+    time.sleep(120)
+    for file_to_upload in os.listdir(path.join('metrics/test_data')):
+        # upload data
+        file_format = file_to_upload.split('.')[-1]
+        upload_metrics[block_size] = {file_format: []}
+        download_metrics[block_size] = {file_format: []}
+        with open(path.join(f'metrics/test_data/{file_to_upload}'), 'rb') as upload_file:
+            mime = magic.Magic(mime=True)
+            upload_start_time = datetime.datetime.now()
+            upload_data = requests.post(f'{APP_URL}/api/file', files={'file': (f'metrics/test_data/{file_to_upload}',
+                                                                                upload_file,
+                                                                                mime.from_file(f'metrics/test_data/{file_to_upload}'))})
+            upload_time = (datetime.datetime.now() - upload_start_time).total_seconds()
+            file_id = upload_data.text
+            upload_metrics[block_size][file_format].append({'time': upload_time,
+                                                            'file_size': os.stat(f'metrics/test_data/{file_to_upload}').st_size})
+        # download data
+        with open(path.join(f'metrics/downloaded_data/{file_id}.{file_format}'), 'wb') as download_file:
+            download_start_time = datetime.datetime.now()
+            download_data = requests.get(f'{APP_URL}/api/file/{file_id}', allow_redirects=True)
+            download_file.write(download_data.content)
+            download_time = (datetime.datetime.now() - download_start_time).total_seconds()
+        download_metrics[block_size][file_format].append({'time': download_time,
+                                                          'file_size': os.stat(f'metrics/downloaded_data/{file_id}.{file_format}').st_size})
+    get_mongo_data = subprocess.run(['docker-compose exec mongodb mongo data_deduplication --quiet --eval "db.stats()"'], capture_output=True, text=True, shell=True)
+    mongo_info = json.loads(get_mongo_data.stdout)
+
+    deduplication_metrics = compute_deduplication(mongo_info['dataSize'])
     block_size *= 2
