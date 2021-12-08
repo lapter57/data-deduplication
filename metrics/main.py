e9b30f11b193ffa796eeb539b4aa257872799d6d
AstralRomance <andrdoncwork@mail.ru>
2021-12-08 17:18:23 +0300

    Add logs and progress bars.

--- metrics/main.py
+++ metrics/main.py
@@ -10,9 +10,11 @@ import uuid
 import magic
 import requests
 import ruamel.yaml
+from loguru import logger
+from progress.bar import IncrementalBar
 
 from aggregate_metrics import compute_deduplication, compute_time, draw_graph, get_dir_size
-from utilities import cleanup, gen_test_data
+from utilities import cleanup
 
 # 'SHA-512'
 # 32768
@@ -24,9 +26,9 @@ work_dir = path.abspath(path.join(__file__, '../../'))
 os.chdir(work_dir)
 
 while get_dir_size('metrics/test_data') < 5368709120:
-    print('Generate new test file')
-    gen_test_data()
-    
+    logger.info(f'Generate test file. Current test data size: {get_dir_size("metrics/test_data")}')
+    os.system(f'head -c {random.randint(1, 30)}M </dev/urandom >metrics/test_data/{uuid.uuid4()}.txt')
+
 
 source_compose = ruamel.yaml.YAML(typ='safe')
 with open('docker-compose.yml', 'r') as src_f:
@@ -34,6 +36,7 @@ with open('docker-compose.yml', 'r') as src_f:
 block_size = 4
 for hash_function in ['SHA-256', 'MURMUR3_128']:
     while block_size <= 65536:
+        logger.info(f'{"="*10}{block_size=}{"="*10}{hash_function=}')
         total_upload_time = []
         total_download_time = []
         for index, env_var in enumerate(source_compose['services']['app']['environment']):
@@ -48,9 +51,11 @@ for hash_function in ['SHA-256', 'MURMUR3_128']:
         print(hash_function)
         os.system('docker-compose up --build -d')
         time.sleep(60)
+        # upload data
+        files_id = []
+        bar = IncrementalBar('Uploading files', max=len(os.listdir(path.join('metrics/test_data'))))
         for file_to_upload in os.listdir(path.join('metrics/test_data')):
-            # upload data
-            file_format = file_to_upload.split('.')[-1]
+            file_format = file_to_upload.split('.')[-1] if len(file_to_upload.split('.')) > 1 else ''
             with open(path.join(f'metrics/test_data/{file_to_upload}'), 'rb') as upload_file:
                 mime = magic.Magic(mime=True)
                 upload_start_time = datetime.datetime.now()
@@ -58,19 +63,27 @@ for hash_function in ['SHA-256', 'MURMUR3_128']:
                                                                                     upload_file,
                                                                                     mime.from_file(f'metrics/test_data/{file_to_upload}'))})
                 upload_time = (datetime.datetime.now() - upload_start_time).total_seconds()
-                total_upload_time.append({'time': upload_time,
-                                        'file_size': os.stat(f'metrics/test_data/{file_to_upload}').st_size})
-                file_id = upload_data.text
-                if file_id[0] == '{':
-                    continue
-            # download data
+                bar.next()
+                if upload_data.status_code // 100 == 2:
+                    total_upload_time.append({'time': upload_time,
+                                              'file_size': os.stat(f'metrics/test_data/{file_to_upload}').st_size})
+                    files_id.append({'file_id': upload_data.text, 'file_format': file_format})
+        # download data
+        bar = IncrementalBar('Downloading files', max=len(files_id))
+        for file_id in files_id:
             with open(path.join(f'metrics/downloaded_data/{file_id}.{file_format}'), 'wb') as download_file:
                 download_start_time = datetime.datetime.now()
-                download_data = requests.get(f'{APP_URL}/api/file/{file_id}', allow_redirects=True)
+                download_data = requests.get(f'{APP_URL}/api/file/{file_id["file_id"]}', allow_redirects=True)
                 download_file.write(download_data.content)
                 download_time = (datetime.datetime.now() - download_start_time).total_seconds()
-                total_download_time.append({'time': download_time,
-                                            'file_size': os.stat(f'metrics/downloaded_data/{file_id}.{file_format}').st_size})
+                bar.next()
+                if download_data.status_code // 100 == 2:
+                    if file_id['file_format']:
+                        total_download_time.append({'time': download_time,
+                                                    'file_size': os.stat(f'metrics/downloaded_data/{file_id["file_id"]}.{file_id["file_format"]}').st_size})
+                    else:
+                        total_download_time.append({'time': download_time,
+                                                    'file_size': os.stat(f'metrics/downloaded_data/{file_id["file_id"]}').st_size})
         get_mongo_data = subprocess.run(['docker-compose exec mongodb mongo data_deduplication --quiet --eval "db.stats()"'], capture_output=True, text=True, shell=True)
         mongo_info = json.loads(get_mongo_data.stdout)
         deduplication_metric = compute_deduplication(mongo_info['dataSize'], block_size, hash_function)
--- metrics/utilities.py
+++ metrics/utilities.py
@@ -9,8 +9,7 @@ import string
 
 def cleanup():
     os.system('docker stop $(docker ps -a -q)')
-    os.system('docker rm $(docker ps -a -q)')
-    os.system('docker rmi $(docker images -a -q)')
+    os.system('docker rm -v mongo')
     files = glob.glob('metrics/blocks/*')
     for file in files:
         if path.isdir(file):
@@ -20,11 +19,3 @@ def cleanup():
     downloaded_files = glob.glob('metrics/downloaded_data/*')
     for file in downloaded_files:
         os.remove(file)
-
-
-def gen_test_data():
-    mb_file_size = random.randint(1, 8)
-    current_file_name = f'metrics/test_data/{uuid.uuid4()}.txt'
-    with open(current_file_name, 'wb') as test_file:
-        while os.fstat(test_file.fileno()).st_size < mb_file_size * 1024**2:
-            test_file.write(str([random.choice(string.ascii_letters) for _ in range(10000)]).encode('utf-8'))
